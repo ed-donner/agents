481a296fa4d3603a2cb1a116478934db5a4c1441
Shmacked <hide579c@gmail.com>
2025-10-24 16:15:47 -0500

    financial researcher updates

--- 3_crew_shane/financial_researcher/src/financial_researcher/crew.py
+++ 3_crew_shane/financial_researcher/src/financial_researcher/crew.py
@@ -1,10 +1,9 @@
 from crewai import Agent, Crew, Process, Task
 from crewai.project import CrewBase, agent, crew, task
 from crewai.agents.agent_builder.base_agent import BaseAgent
+from crewai_tools import SerperDevTool
 from typing import List
-# If you want to run a snippet of code before or after the crew starts,
-# you can use the @before_kickoff and @after_kickoff decorators
-# https://docs.crewai.com/concepts/crews#example-crew-class-with-decorators
+
 
 @CrewBase
 class FinancialResearcher():
@@ -18,7 +17,7 @@ class FinancialResearcher():
 
     @agent
     def researcher(self) -> Agent:
-        return Agent(config=self.agents_config['researcher'], verbose=True)
+        return Agent(config=self.agents_config['researcher'], verbose=True, tools=[SerperDevTool()])
 
     @agent
     def analyst(self) -> Agent:
