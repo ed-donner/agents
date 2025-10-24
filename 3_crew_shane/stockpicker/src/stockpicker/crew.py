aa17fc911860334d75fcea17a7c610d957a1ba47
Shmacked <hide579c@gmail.com>
2025-10-24 16:34:12 -0500

    update the crew

--- 3_crew_shane/stockpicker/src/stockpicker/crew.py
+++ 3_crew_shane/stockpicker/src/stockpicker/crew.py
@@ -2,63 +2,67 @@ from crewai import Agent, Crew, Process, Task
 from crewai.project import CrewBase, agent, crew, task
 from crewai.agents.agent_builder.base_agent import BaseAgent
 from typing import List
-# If you want to run a snippet of code before or after the crew starts,
-# you can use the @before_kickoff and @after_kickoff decorators
-# https://docs.crewai.com/concepts/crews#example-crew-class-with-decorators
+from crewai_tools import SerperDevTool
+from pydantic import BaseModel, Field
+
+
+class TrendingCompany(BaseModel):
+    """ A company that is in the news and attracting attention """
+    name: str = Field(description="Company name")
+    ticker: str = Field(description="Stock ticker symbol")
+    reason: str = Field(description="Reason this company is trending in the news")
+
+
+class TrendingCompanyList(BaseModel):
+    """ A list of trending companies """
+    companies: List[TrendingCompany] = Field(description="List of trending companies")
+
+
+class TrendingCompanyResearch(BaseModel):
+    """ Detailed research on a trending company """
+    name: str = Field(description="Company name")
+    market_position: str = Field(description="Current market position and competitive analysis")
+    future_outlook: str = Field(description="Future outlook and growth prospects")
+    investment_potential: str = Field(description="Investment potential and suitability for investment")
+
+
+class TrendingCompanyResearchList(BaseModel):
+    """ A list of researched trending companies """
+    companies: List[TrendingCompanyResearch] = Field(description="List of researched trending companies")
+
 
 @CrewBase
 class Stockpicker():
     """Stockpicker crew"""
+    agents_config: "config/agents.yaml"
+    tasks_config: "config/tasks.yaml"
 
-    agents: List[BaseAgent]
-    tasks: List[Task]
-
-    # Learn more about YAML configuration files here:
-    # Agents: https://docs.crewai.com/concepts/agents#yaml-configuration-recommended
-    # Tasks: https://docs.crewai.com/concepts/tasks#yaml-configuration-recommended
+    @agent
+    def trending_company_finder(self) -> Agent:
+        return Agent(config=self.agents_config['trending_company_finder'], tools=[SerperDevTool()])
     
-    # If you would like to add tools to your agents, you can learn more about it here:
-    # https://docs.crewai.com/concepts/agents#agent-tools
     @agent
-    def researcher(self) -> Agent:
-        return Agent(
-            config=self.agents_config['researcher'], # type: ignore[index]
-            verbose=True
-        )
-
+    def financial_researcher(self) -> Agent:
+        return Agent(config=self.agents_config['financial_researcher'], tools=[SerperDevTool()])
+    
     @agent
-    def reporting_analyst(self) -> Agent:
-        return Agent(
-            config=self.agents_config['reporting_analyst'], # type: ignore[index]
-            verbose=True
-        )
-
-    # To learn more about structured task outputs,
-    # task dependencies, and task callbacks, check out the documentation:
-    # https://docs.crewai.com/concepts/tasks#overview-of-a-task
+    def stock_picker(self) -> Agent:
+        return Agent(config=self.agents_config['stock_picker'])
+    
     @task
-    def research_task(self) -> Task:
-        return Task(
-            config=self.tasks_config['research_task'], # type: ignore[index]
-        )
-
+    def find_trending_companies(self) -> Task:
+        return Task(config=self.tasks_config['find_trending_companies'], output_pydantic=TrendingCompanyList)
+    
     @task
-    def reporting_task(self) -> Task:
-        return Task(
-            config=self.tasks_config['reporting_task'], # type: ignore[index]
-            output_file='report.md'
-        )
+    def research_trending_companies(self) -> Task:
+        return Task(config=self.tasks_config['research_trending_companies'], output_pydantic=TrendingCompanyResearchList)
+    
+    @task
+    def pick_best_company(self) -> Task:
+        return Task(config=self.tasks_config['pick_best_company'])
 
     @crew
     def crew(self) -> Crew:
-        """Creates the Stockpicker crew"""
-        # To learn how to add knowledge sources to your crew, check out the documentation:
-        # https://docs.crewai.com/concepts/knowledge#what-is-knowledge
+        manager = Agent(config=self.agents_config['manager'], allow_delegation=True)
+        return Crew(agents=self.agents, tasks=self.tasks, process=Process.hierarchical, verbose=True, manager_agent=manager)
 
-        return Crew(
-            agents=self.agents, # Automatically created by the @agent decorator
-            tasks=self.tasks, # Automatically created by the @task decorator
-            process=Process.sequential,
-            verbose=True,
-            # process=Process.hierarchical, # In case you wanna use that instead https://docs.crewai.com/how-to/Hierarchical/
-        )
