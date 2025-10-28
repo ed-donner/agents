5bc02c52ab1b2e75999711c4f1101e8d67e52ce5
Amir Beheshti <amirmohammad.beheshti@gmail.com>
2025-10-28 11:50:07 +0330

    start open ai agent sdk

---
+++ 1_foundations/README.md
@@ -0,0 +1,6 @@
+---
+title: 1_foundations
+app_file: deploy_app.py
+sdk: gradio
+sdk_version: 5.34.2
+---
--- 1_foundations/deploy_app.py
+++ 1_foundations/deploy_app.py
@@ -1,33 +1,23 @@
+# If you don't know what any of these packages do - you can always ask ChatGPT for a guide!
+
 from dotenv import load_dotenv
 from openai import OpenAI
-import json
-import os
-import requests
 from pypdf import PdfReader
 import gradio as gr
-
+import os 
+import json
+import requests
 
 load_dotenv(override=True)
 
-def push(text):
-    requests.post(
-        "https://api.pushover.net/1/messages.json",
-        data={
-            "token": os.getenv("PUSHOVER_TOKEN"),
-            "user": os.getenv("PUSHOVER_USER"),
-            "message": text,
-        }
-    )
+# Print the key prefixes to help with any debugging
 
+google_api_key = os.getenv('GOOGLE_API_KEY')
 
-def record_user_details(email, name="Name not provided", notes="not provided"):
-    push(f"Recording {name} with email {email} and notes {notes}")
-    return {"recorded": "ok"}
-
-def record_unknown_question(question):
-    push(f"Recording {question}")
-    return {"recorded": "ok"}
-
+if google_api_key:
+    print(f"Google API Key exists and begins {google_api_key[:2]}")
+    
+    
 record_user_details_json = {
     "name": "record_user_details",
     "description": "Use this tool to record that a user is interested in being in touch and provided an email address",
@@ -72,12 +62,30 @@ record_unknown_question_json = {
 tools = [{"type": "function", "function": record_user_details_json},
         {"type": "function", "function": record_unknown_question_json}]
 
+def push(text):
+    requests.post(
+        "https://api.pushover.net/1/messages.json",
+        data={
+            "token": os.getenv("PUSHOVER_TOKEN"),
+            "user": os.getenv("PUSHOVER_USER"),
+            "message": text,
+        }
+    )
+    
+def record_user_details(email, name="Name not provided", notes="not provided"):
+    push(f"Recording {name} with email {email} and notes {notes}")
+    return {"recorded": "ok"}
+
+def record_unknown_question(question):
+    push(f"Recording {question}")
+    return {"recorded": "ok"}
 
 class Me:
-
+    name = 'Amir Mohammad Beheshti'
+    
     def __init__(self):
-        self.openai = OpenAI()
-        self.name = "Ed Donner"
+        self.openai = OpenAI(api_key=google_api_key, base_url="https://generativelanguage.googleapis.com/v1beta/openai/" )
+        self.name = "Amir Mohammad Beheshti"
         reader = PdfReader("me/linkedin.pdf")
         self.linkedin = ""
         for page in reader.pages:
@@ -86,8 +94,22 @@ class Me:
                 self.linkedin += text
         with open("me/summary.txt", "r", encoding="utf-8") as f:
             self.summary = f.read()
+    
+    def system_prompt(self):
+        system_prompt = f"You are acting as {self.name}. You are answering questions on {self.name}'s website, \
+    particularly questions related to {self.name}'s career, background, skills and experience. \
+    Your responsibility is to represent {self.name} for interactions on the website as faithfully as possible. \
+    You are given a summary of {self.name}'s background and LinkedIn profile which you can use to answer questions. \
+    Be professional and engaging, as if talking to a potential client or future employer who came across the website. \
+    If you don't know the answer to any question, use your record_unknown_question tool to record the question that you couldn't answer, even if it's about something trivial or unrelated to career. \
+    If the user is engaging in discussion, try to steer them towards getting in touch via email; ask for their email and record it using your record_user_details tool. "
 
-
+        system_prompt += f"\n\n## Summary:\n{self.summary}\n\n## LinkedIn Profile:\n{self.linkedin}\n\n"
+        system_prompt += f"With this context, please chat with the user, always staying in character as {self.name}."
+        
+        return system_prompt
+    
+    
     def handle_tool_call(self, tool_calls):
         results = []
         for tool_call in tool_calls:
@@ -99,26 +121,16 @@ class Me:
             results.append({"role": "tool","content": json.dumps(result),"tool_call_id": tool_call.id})
         return results
     
-    def system_prompt(self):
-        system_prompt = f"You are acting as {self.name}. You are answering questions on {self.name}'s website, \
-particularly questions related to {self.name}'s career, background, skills and experience. \
-Your responsibility is to represent {self.name} for interactions on the website as faithfully as possible. \
-You are given a summary of {self.name}'s background and LinkedIn profile which you can use to answer questions. \
-Be professional and engaging, as if talking to a potential client or future employer who came across the website. \
-If you don't know the answer to any question, use your record_unknown_question tool to record the question that you couldn't answer, even if it's about something trivial or unrelated to career. \
-If the user is engaging in discussion, try to steer them towards getting in touch via email; ask for their email and record it using your record_user_details tool. "
-
-        system_prompt += f"\n\n## Summary:\n{self.summary}\n\n## LinkedIn Profile:\n{self.linkedin}\n\n"
-        system_prompt += f"With this context, please chat with the user, always staying in character as {self.name}."
-        return system_prompt
-    
     def chat(self, message, history):
         messages = [{"role": "system", "content": self.system_prompt()}] + history + [{"role": "user", "content": message}]
         done = False
         while not done:
-            print(f"messages: {messages[len(messages) - 1]}", flush=True )
-            response = self.openai.chat.completions.create(model="gpt-4o-mini", messages=messages, tools=tools)
-            print(f"finish_reason: {response.choices[0].finish_reason}",  flush=True)
+            client = OpenAI(api_key=google_api_key, base_url="https://generativelanguage.googleapis.com/v1beta/openai/"  )
+            response = client.chat.completions.create(
+                model="gemini-2.0-flash",
+                messages=messages,
+                tools=tools
+            )
             if response.choices[0].finish_reason=="tool_calls":
                 message = response.choices[0].message
                 tool_calls = message.tool_calls
@@ -129,7 +141,7 @@ If the user is engaging in discussion, try to steer them towards getting in touc
                 done = True
         return response.choices[0].message.content
     
-
+    
 if __name__ == "__main__":
     me = Me()
     gr.ChatInterface(me.chat, type="messages").launch()
