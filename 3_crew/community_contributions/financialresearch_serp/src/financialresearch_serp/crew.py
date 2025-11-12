f46d9e6a19dc2c6c5057ee5b2a91db4e046aa4cb
Oluwatosin Oyeladun <toyeladun@gmail.com>
2025-11-12 13:46:03 +0000

    crew ai set up and saved

--- 3_crew/community_contributions/financialresearch_serp/src/financialresearch_serp/crew.py
+++ 3_crew/community_contributions/financialresearch_serp/src/financialresearch_serp/crew.py
@@ -10,55 +10,43 @@ from typing import List
 class FinancialresearchSerp():
     """FinancialresearchSerp crew"""
 
-    agents: List[BaseAgent]
-    tasks: List[Task]
+    #agents: List[BaseAgent]
+    #tasks: List[Task]
 
-    # Learn more about YAML configuration files here:
-    # Agents: https://docs.crewai.com/concepts/agents#yaml-configuration-recommended
-    # Tasks: https://docs.crewai.com/concepts/tasks#yaml-configuration-recommended
-    
-    # If you would like to add tools to your agents, you can learn more about it here:
-    # https://docs.crewai.com/concepts/agents#agent-tools
+    agents_config='config/agents.yaml'
+    tasks_config='config/tasks.yaml'
+
     @agent
     def researcher(self) -> Agent:
         return Agent(
-            config=self.agents_config['researcher'], # type: ignore[index]
-            verbose=True
-        )
+            config=self.agents_config['researcher'],
+            verbose=True)
 
     @agent
-    def reporting_analyst(self) -> Agent:
+    def analyst(self) -> Agent:
         return Agent(
-            config=self.agents_config['reporting_analyst'], # type: ignore[index]
+            config=self.agents_config['analyst'], 
             verbose=True
         )
 
-    # To learn more about structured task outputs,
-    # task dependencies, and task callbacks, check out the documentation:
-    # https://docs.crewai.com/concepts/tasks#overview-of-a-task
+
+
     @task
     def research_task(self) -> Task:
         return Task(
-            config=self.tasks_config['research_task'], # type: ignore[index]
-        )
+            config=self.tasks_config['research_task'])
+
 
     @task
-    def reporting_task(self) -> Task:
+    def analysis_task(self) -> Task:
         return Task(
-            config=self.tasks_config['reporting_task'], # type: ignore[index]
-            output_file='report.md'
-        )
+            config=self.tasks_config['analysis_task'])
 
     @crew
     def crew(self) -> Crew:
-        """Creates the FinancialresearchSerp crew"""
-        # To learn how to add knowledge sources to your crew, check out the documentation:
-        # https://docs.crewai.com/concepts/knowledge#what-is-knowledge
-
         return Crew(
-            agents=self.agents, # Automatically created by the @agent decorator
-            tasks=self.tasks, # Automatically created by the @task decorator
+            agents=self.agents,
+            tasks=self.tasks,
             process=Process.sequential,
             verbose=True,
-            # process=Process.hierarchical, # In case you wanna use that instead https://docs.crewai.com/how-to/Hierarchical/
         )
