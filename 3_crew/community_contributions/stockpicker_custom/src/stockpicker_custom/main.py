77061babb5533f7227ea833ade9721845089aa1c
Oluwatosin Oyeladun <toyeladun@gmail.com>
2025-12-15 16:10:03 +0000

    Main set up to run

--- 3_crew/community_contributions/stockpicker_custom/src/stockpicker_custom/main.py
+++ 3_crew/community_contributions/stockpicker_custom/src/stockpicker_custom/main.py
@@ -8,61 +8,63 @@ from stockpicker_custom.crew import StockpickerCustom
 
 warnings.filterwarnings("ignore", category=SyntaxWarning, module="pysbd")
 
-# This main file is intended to be a way for you to run your
-# crew locally, so refrain from adding unnecessary logic into this file.
-# Replace with inputs you want to test with, it will automatically
-# interpolate any tasks and agents information
+
 
 def run():
     """
-    Run the crew.
+    Run the Research Crew.
     """
     inputs = {
-        'topic': 'AI LLMs',
-        'current_year': str(datetime.now().year)
+        'sector': 'technology',
+        'region': 'Africa'
     }
-    
-    try:
-        StockpickerCustom().crew().kickoff(inputs=inputs)
-    except Exception as e:
-        raise Exception(f"An error occurred while running the crew: {e}")
+    result = StockpickerCustom().crew().kickoff(inputs=inputs)
+
+    print("\n\n=== FINAL DECISION ===\n\n")
+    print(result.raw)
+
+if __name__ == "__main__":
+    run()
 
 
-def train():
-    """
-    Train the crew for a given number of iterations.
-    """
-    inputs = {
-        "topic": "AI LLMs",
-        'current_year': str(datetime.now().year)
-    }
-    try:
-        StockpickerCustom().crew().train(n_iterations=int(sys.argv[1]), filename=sys.argv[2], inputs=inputs)
 
-    except Exception as e:
-        raise Exception(f"An error occurred while training the crew: {e}")
 
-def replay():
-    """
-    Replay the crew execution from a specific task.
-    """
-    try:
-        StockpickerCustom().crew().replay(task_id=sys.argv[1])
 
-    except Exception as e:
-        raise Exception(f"An error occurred while replaying the crew: {e}")
 
-def test():
-    """
-    Test the crew execution and returns the results.
-    """
-    inputs = {
-        "topic": "AI LLMs",
-        "current_year": str(datetime.now().year)
-    }
-    
-    try:
-        StockpickerCustom().crew().test(n_iterations=int(sys.argv[1]), eval_llm=sys.argv[2], inputs=inputs)
 
-    except Exception as e:
-        raise Exception(f"An error occurred while testing the crew: {e}")
+
+
+
+
+
+
+
+
+
+
+
+
+
+inputs=inputs)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+nputs=inputs)
+
