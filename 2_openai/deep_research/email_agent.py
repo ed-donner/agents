8f3c437d6b475714135769757fa508aae4dd735d
Daniel David <dd3269@columbia.edu>
2026-02-11 09:31:21 -0500

    fix: update email sender and recipient addresses in email_agent.py
    Running the Deeep research agentic flow via Gradion UI and send report to my email adderess

--- 2_openai/deep_research/email_agent.py
+++ 2_openai/deep_research/email_agent.py
@@ -10,8 +10,8 @@ from agents import Agent, function_tool
 def send_email(subject: str, html_body: str) -> Dict[str, str]:
     """Send an email with the given subject and HTML body"""
     sg = sendgrid.SendGridAPIClient(api_key=os.environ.get("SENDGRID_API_KEY"))
-    from_email = Email("ed@edwarddonner.com")  # put your verified sender here
-    to_email = To("ed.donner@gmail.com")  # put your recipient here
+    from_email = Email("daniel1957000@gmail.com" , "Daniel David")  # put your verified sender here
+    to_email = To("dd3269@columbia.edu")  # put your recipient here
     content = Content("text/html", html_body)
     mail = Mail(from_email, to_email, subject, content).get()
     response = sg.client.mail.send.post(request_body=mail)
