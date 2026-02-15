80db8364b3d5de46b883d78c61d9d680133bc814
TiagoIesbick <tiagoiesbick@gmail.com>
2026-02-14 21:40:36 -0300

    Address PR feedback

--- 4_langgraph/community_contributions/langgraph-sidekick/db/sql_memory.py
+++ 4_langgraph/community_contributions/langgraph-sidekick/db/sql_memory.py
@@ -1,12 +1,8 @@
-import os
 import aiosqlite
 from langgraph.checkpoint.sqlite.aio import AsyncSqliteSaver
 
 
 async def setup_memory(db_path: str = "db/memory.db") -> AsyncSqliteSaver:
-    """Initialize Async SQLite memory storage."""
-    #os.makedirs(db_path, exist_ok=True)
-    # open async connection
     conn = await aiosqlite.connect(db_path)
     await conn.execute("PRAGMA journal_mode=WAL;")
     await conn.commit()
