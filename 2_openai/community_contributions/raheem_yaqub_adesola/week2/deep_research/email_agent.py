6d5b83452d580f46859677d64643415ca1c40e82
yaqubadesola <yaqub.adesola@gmail.com>
2026-03-28 22:36:25 +0100

    Removing hardcoded emails

--- 2_openai/community_contributions/raheem_yaqub_adesola/week2/deep_research/email_agent.py
+++ 2_openai/community_contributions/raheem_yaqub_adesola/week2/deep_research/email_agent.py
@@ -9,8 +9,8 @@ from agents import Agent, function_tool
 def send_email(subject: str, html_body: str) -> Dict[str, str]:
     """Send an email with the given subject and HTML body"""
     sg = sendgrid.SendGridAPIClient(api_key=os.environ.get("SENDGRID_API_KEY"))
-    from_email = Email("yaqub.adesola.com")  # put your verified sender here
-    to_email = To("yakdseeker@gmail.com")  # put your recipient here
+    from_email = Email(os.environ.get("FROM_EMAIL"))# put your verified sender here
+    to_email = To(os.environ.get("TO_EMAIL"))  # put your recipient here
     content = Content("text/html", html_body)
     mail = Mail(from_email, to_email, subject, content).get()
     response = sg.client.mail.send.post(request_body=mail)
