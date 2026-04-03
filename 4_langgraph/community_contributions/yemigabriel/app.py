a2baaf6082554d20c023f0990e084ceaa7a30114
yemi gabriel <yemig@banquemisr.com>
2026-04-03 17:58:48 +0100

    fix: fixes error when user inputs before `run_superstep` is complete

--- 4_langgraph/community_contributions/yemigabriel/app.py
+++ 4_langgraph/community_contributions/yemigabriel/app.py
@@ -2,20 +2,25 @@ import gradio as gr
 from sidekick import Sidekick
 
 
-async def setup():
+async def create_sidekick():
     sidekick = Sidekick()
     await sidekick.setup()
     return sidekick
 
 
+async def setup():
+    return await create_sidekick()
+
+
 async def process_message(sidekick, message, success_criteria, history):
+    if sidekick is None:
+        sidekick = await create_sidekick()
     results = await sidekick.run_superstep(message, success_criteria, history)
     return results, sidekick
 
 
 async def reset():
-    new_sidekick = Sidekick()
-    await new_sidekick.setup()
+    new_sidekick = await create_sidekick()
     return "", "", None, new_sidekick
 
 
