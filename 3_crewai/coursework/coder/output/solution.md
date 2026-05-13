796ddd2cd9b3ba5753c3139ee3f3d3c3d7761f1a
Edward Donner <ed.donner@gmail.com>
2026-05-13 12:02:55 +0100

    Week 3 day 4 updated

---
+++ 3_crewai/coursework/coder/output/solution.md
@@ -0,0 +1,8 @@
+I wrote a Python program in the sandbox to compute the first 1,000,000 terms of the Leibniz series for π:
+
+1 - 1/3 + 1/5 - 1/7 + ...
+
+The program multiplies the sum by 4 and prints the result.
+
+Final output:
+3.1415916535897743
--- 3_crewai/reference/engineering_team/src/engineering_team/crew.py
+++ 3_crewai/reference/engineering_team/src/engineering_team/crew.py
@@ -80,5 +80,6 @@ class EngineeringTeam():
             tasks=self.tasks, # Automatically created by the @task decorator
             process=Process.sequential,
             verbose=True,
+            tracing=True,
             # process=Process.hierarchical, # In case you wanna use that instead https://docs.crewai.com/how-to/Hierarchical/
         )
